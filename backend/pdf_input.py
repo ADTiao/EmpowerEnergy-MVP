a8c8bd0649cf3fcba0bad3c9ca7a553252544760
Aaron Tiao <aarontiao@Aarons-MacBook-Air-7.local>
2025-07-23 11:29:38 +0300

    sample pdf added

---
+++ backend/pdf_input.py
@@ -0,0 +1,11 @@
+## goal is to receive a pdf and then extract the information
+
+
+""" tasks: 
+
+1) Receive input from a pdf
+2) parse through the pdf
+3) clean for only the critical information
+
+"""
+
