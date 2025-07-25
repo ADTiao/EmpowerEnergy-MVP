4ff198864d53cf1e55650b55c8dd9a17f45564ac
Aaron Tiao <aarontiao@Aarons-MacBook-Air-7.local>
2025-07-25 18:09:50 +0300

    added gen_summary, fixed gen_info

---
+++ backend/gen_info.py
@@ -0,0 +1,95 @@
+import llm_reply
+import json
+import math
+
+proposal = {
+    "company" : "EmpowerEnergy",
+    "village" : "Nairobi",
+    "longitude" : -17.91782,
+    "latitude" : 30.90988,
+    "start_date" : "January 7, 2025",
+    "duration" : 10,
+    "capex" : 10,
+    "opex" : 10,
+    "sus_plan" : "Here is my sustainability plan",
+    "total_cost" : 8,
+    "num_houses" : 1000,
+    "co2" : 1000,
+    "pue" : "There will be many productive uses of energy",
+    "ppl" : 500
+}
+
+criteria = {
+    "duration" : 150,
+    "capex" : 400,
+    "opex" : 400,
+    "total_cost" : 900,
+    "num_houses" : 70,
+    "co2" : 500,
+    "ppl" : 250
+}
+
+weights = {
+    "duration" : .10,
+    "capex" : 0,
+    "opex" : 0,
+    "total_cost" : .40,
+    "num_houses" : .20,
+    "co2" : .10,
+    "ppl" : .20
+}
+
+feedback = {
+    "duration" : "the duration of your project exceeds the requirement for our funding.",
+    "capex" : "the capital expenditures for your project are too high",
+    "opex" : "the operational expenditures for your project are too high",
+    "total_cost" : "the total requested funds for your project are too high",
+    "num_houses" : "the number of connections for your project is too low",
+    "co2" : "the avoided co2 emissions projected for your project are too low",
+    "ppl" : "the number of people impacted by your project is too low"
+}
+
+def get_prop(bool, basic_prop):
+    if bool == True:
+        response = llm_reply.main()
+        llm_info = json.loads(response)
+        return llm_info
+    else:
+        return basic_prop
+
+def scoring(proposal, criteria, weights):
+    # missing the qualitative description comparisons
+    score = 0
+    count = 0
+    prop_crit = []
+    null_crit = []
+    num_metric = len(criteria)    
+    for key in criteria.keys(): 
+        count += 1
+        if proposal[key] == None:
+            score -= weights[key] * num_metric
+            null_crit.append(f"Proposal is missing information regarding the {key}")
+        elif count <= 4:
+            if proposal[key] <= criteria[key]:
+                score += weights[key] * num_metric
+            else: 
+                prop_crit.append(feedback[key])
+        else: 
+            if proposal[key] >= criteria[key]:
+                score += weights[key] * num_metric
+            else: 
+                prop_crit.append(feedback[key])
+    end_score = (score/num_metric) * 100
+    return math.trunc(end_score), prop_crit, null_crit
+
+def main():
+    bool = False
+    raw_prop_info = get_prop(bool, proposal)
+    prop_feedback = scoring(raw_prop_info, criteria, weights)
+    score = prop_feedback[0]
+    value_feedback = prop_feedback[1]
+    null_feedback = prop_feedback[2]
+    return score, value_feedback, null_feedback, raw_prop_info
+
+if __name__ == "__main__":
+    main()
---
+++ backend/gen_summary.py
@@ -0,0 +1,94 @@
+import json
+import gen_info
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
+
+# get score and proposal information
+info = gen_info.main()
+score = info[0]
+miss_feed = info[1]
+null_feed = info[2]
+prop_info = info[3]
+    
+def dev_feedback(score, null_feed, miss_feed):
+   num_null = len(null_feed)
+   num_miss = len(miss_feed)
+   null_script = ""
+   miss_script = ""
+   if num_null != 0:
+        null_script = "\nYour proposal did not include key pieces of information: \n"
+        for i in range(num_null):
+            null_script += f"     {i+1}) {null_feed[i]}"
+   if num_miss != 0:
+        miss_script = "\nYour proposal did not meet the funding criteria in the following ways: \n"
+        for i in range(num_miss):
+            miss_script += f"     {i+1}) {miss_feed[i]}"  
+   if num_null != 0 and num_miss != 0:     
+        feedback = f"Below is some insight as to why your received this score:\n{miss_script}{null_script}" 
+   else: 
+        feedback = "Great Job!"
+   script = f"Your proposal received a score of {score}. {feedback}"  
+   return script 
+
+# API info
+url = "https://openrouter.ai/api/v1/chat/completions"
+key = os.getenv("OPEN_API_KEY")
+headers = {
+    "Authorization": f"Bearer {key}",
+    "Content-Type": "application/json"
+    }
+prompt = f"""
+
+Please provide the following summary of an energy access proposal using the following instructions: 
+
+Use the following format: 
+
+Company Name
+Village Name
+Longitude, latitude
+Score: use the score value inputed here -- {score}
+
+First Paragraph {prop_info}:
+One sentence with information about the duration (start date, duration)
+One sentence with the capital expenditures (capex), operational expenditures (opex), and the total requested funds (total_cost)
+One sentence summary of the sustainability plan
+Two-three sentences about the impact. Include information about the carbon emissions avoided (c02), 
+number of connections (num_houses), number of people impacted (num_ppl), and the productive uses of energy
+that will be brought about because of the project.
+
+Second Paragraph: 
+One or two sentences detailing missing information {null_feed}
+One or two sentences detailing where the project projections do not fulfill the criteria according
+to {miss_feed}. Do not provide analysis, simply state where it does not match. 
+
+DO NOT PROVIDE YOUR OWN ANALYSIS. Simply state the data in legible form and that is all. Additionally, do not
+put the template provided in your response. Only the answer.  
+"""
+
+data = {
+        "model" : "meta-llama/llama-3-8b-instruct",
+        "messages" : [ {
+            "role" : "system",
+            "content" : 
+            """
+You are acting on the behalf of a rural electrification financier. In this role, you will be 
+summarizing a project proposal using the information given to you. 
+            """
+            },
+            {
+            "role " : "user", 
+            "content" : json.dumps(prompt)
+            }
+        ]
+    }
+
+def summarize(url, headers, data):
+    answer = requests.post(url, headers=headers, json=data)
+    response = answer.json()["choices"][0]["message"]["content"]
+    return response
+
+if __name__ == "__main__":
+    # summarize(url, headers, data)
+    print(dev_feedback(score, null_feed, miss_feed))
